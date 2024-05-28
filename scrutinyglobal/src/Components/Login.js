f2394361efccb482bbd7a2554cef42e8515e8364
Bhanvi Gautam <bhanvigautam@gmail.com>
2024-05-28 09:38:51 +0530

    signup-2

--- scrutinyglobal/src/Components/Login.js
+++ scrutinyglobal/src/Components/Login.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -9,6 +10,7 @@ import {
   FormHelperText,
   Grid,
   InputLabel,
+  Stack,
   TextField,
   Typography,
   styled,
@@ -16,6 +18,8 @@ import {
 import { useState, Fragment } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../logo_sg.png";
+import Google from "../Google.svg";
+import AppleIcon from "../AppleIcon.svg";
 import backgroundImage from "../background.png";
 import "./Login.css";
 
@@ -47,26 +51,59 @@ const Login = (props) => {
       alignItems="center"
       style={{
         height: "100vh",
+        background: "rgb(83,89,231)",
+        background:
+          "linear-gradient(0deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
       }}
     >
-      <Grid item xs={6} md={5} justifyContent="center" alignItems="center">
-        <Card style={{ border: "2px solid black" }}>
-          <CardContent sx={{ color: "black" }}>
-            Hello !Welcome to <br />
-            Srunity Global
+      <Grid
+        item
+        xs={6}
+        md={5}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "inherit" }}
+      >
+        <Card
+          elevation="0"
+          style={{
+            height: "100vh",
+            background: "rgb(83,89,231)",
+            background:
+              "linear-gradient(0deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
+          }}
+        >
+          <CardContent sx={{ color: "white", m: 4 }}>
+            <div className="vollkorn-hello">Hello !Welcome to </div>
+            <div className="vollkorn-scrutiny">Srunity Global</div>
           </CardContent>
 
           <CardMedia
             component="img"
             image={backgroundImage}
-            sx={{ width: "20vh", borderRadius: "50%" }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              width: "42vh",
+              height: "42vh",
+              borderRadius: "50%",
+              backgroundColor: "#0cdaff",
+            }}
           />
+
+          <CardContent sx={{ color: "white" }}>
+            <div className="vollkorn-body">
+              A CRM for CAWI project management
+            </div>
+          </CardContent>
         </Card>
       </Grid>
       <Grid item xs={6} md={7}>
         <Card
           style={{
-            border: "2px solid black",
+            // border: "2px solid black",
             borderTopRightRadius: 0,
             borderBottomRightRadius: 0,
             borderTopLeftRadius: "10%",
@@ -75,10 +112,10 @@ const Login = (props) => {
           sx={{
             px: 2,
             height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            // display: "flex",
+            // flexDirection: "column",
+            // justifyContent: "center",
+            // alignItems: "center",
           }}
         >
           <CardMedia
@@ -98,12 +135,12 @@ const Login = (props) => {
             SCRUNITY GLOBAL
           </Typography> */}
 
-          <CardContent>
+          <CardContent sx={{ mt: 9, mx: 9, mb: 3 }}>
             <form onSubmit={handleSubmit}>
               <InputLabel
                 htmlFor="emailOrNumber"
                 className="arimo-font"
-                sx={{ m: 1 }}
+                sx={{ mt: 3 }}
               >
                 Email or Number
               </InputLabel>
@@ -116,12 +153,13 @@ const Login = (props) => {
                 onChange={onChangeUsername}
                 margin="normal"
                 size="small"
+                fullWidth
                 required
               />
               <InputLabel
                 htmlFor="password"
                 className="arimo-font"
-                sx={{ m: 1 }}
+                sx={{ mt: 3 }}
               >
                 Password
               </InputLabel>
@@ -135,31 +173,91 @@ const Login = (props) => {
                 onChange={onChangePassword}
                 margin="normal"
                 size="small"
+                fullWidth
                 required
               />
-              <FormHelperText sx={{ color: "blue", mb: 2 }}>
+              <FormHelperText sx={{ color: "#175f93", mb: 2 }}>
                 forgot password?
               </FormHelperText>
 
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                sx={{ mt: 1 }}
-              >
-                Sign In
-              </Button>
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    width: "40%",
+                    mt: 1,
+                    background: "rgb(83,89,231)",
+                    background:
+                      "linear-gradient(274deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
+                  }}
+                >
+                  <Typography
+                    variant="body"
+                    fontWeight="bold"
+                    style={{ fontSize: "20px" }}
+                  >
+                    Sign In
+                  </Typography>
+                </Button>
+              </Box>
             </form>
           </CardContent>
 
-          <Divider sx={{ m: 2 }}>Or</Divider>
-          <Grid>
-            <Button>Sign In with Google</Button>
-            <Button>Sign In with Apple</Button>
-          </Grid>
-          <Grid justifyContent="center" alignItems="center">
-            <CardActions sx={{ color: "white" }}>
-              Are you new? <Link to="/register">Sign up here</Link>
+          <Divider sx={{ p: 1 }}>Or</Divider>
+          <Stack
+            direction="row"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            spacing={9}
+            sx={{ my: 4 }}
+          >
+            <Button
+              startIcon={<img src={Google} alt="Google" />}
+              variant="contained"
+              sx={{
+                backgroundColor: "#d8d5d5",
+                color: "black",
+                boxShadow: "3px 3px 4px 0px rgba(0,0,0,0.25)",
+              }}
+            >
+              Sign In with Google
+            </Button>
+            <Button
+              startIcon={
+                <img src={AppleIcon} alt="Apple" style={{ height: "5vh" }} />
+              }
+              variant="contained"
+              sx={{
+                backgroundColor: "#d8d5d5",
+                color: "black",
+                boxShadow: "3px 3px 4px 0px rgba(0,0,0,0.25)",
+              }}
+            >
+              {" "}
+              Sign In with Apple
+            </Button>
+          </Stack>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            // sx={{ m: 1 }}
+          >
+            <CardActions>
+              {"Are you new?   "}
+              <Link
+                to="/register"
+                style={{
+                  textDecoration: "none",
+                  fontWeight: "bold",
+                  color: "#0cdaff",
+                }}
+              >
+                Sign up here
+              </Link>
             </CardActions>
           </Grid>
         </Card>
