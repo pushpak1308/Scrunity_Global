a291568aec2826f2209da7493b891b58c2d94e69
Bhanvi Gautam <bhanvigautam@gmail.com>
2024-05-31 00:38:45 +0530

    signup-5

--- scrutinyglobal/src/Components/Login.js
+++ scrutinyglobal/src/Components/Login.js
@@ -14,7 +14,7 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../logo_sg.png";
 import backgroundImage from "../background.png";
 import "./Login.css";
-import { CustomInputLabel, CustomTextField } from "./Parts/CustomTextField";
+import { CustomTextField } from "./Parts/CustomTextField";
 
 const Login = (props) => {
   const [username, setUsername] = useState("");
@@ -38,189 +38,197 @@ const Login = (props) => {
   };
 
   return (
-    <Grid
-      container
-      style={{
-        border:"2px solid black",
-        background: "rgb(83,89,231)",
-        background:
-          "linear-gradient(0deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
-      }}
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      style={{ backgroundColor: "#abeff5" }}
     >
       <Grid
-        item
-        xs={6}
-        md={5}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
+        container
+        style={{
+          borderRadius: "10px",
+          overflow: "hidden",
+          width: "80%",
+          background: "rgb(83,89,231)",
+          background:
+            "linear-gradient(0deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
+        }}
       >
-        <Card
-          elevation="0"
-          style={{
-            background: "rgb(83,89,231)",
-            background:
-              "linear-gradient(0deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
-
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column'
-
-          }}
+        <Grid
+          item
+          xs={12}
+          md={5}
+          spacing={0}
+          display={{ xs: "none", md: "flex" }}
+          justifyContent="center"
+          alignItems="center"
         >
-          <CardContent sx={{ color: "white", mb: 2 }}>
-            <div className="vollkorn-hello">Hello !Welcome to </div>
-            <div className="vollkorn-scrutiny">Srunity Global</div>
-          </CardContent>
-
-          <CardMedia
-            component="img"
-            image={backgroundImage}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              width: "42vh",
-              height: "42vh",
-              borderRadius: "50%",
-              backgroundColor: "#0cdaff",
-              ml: 6,
-              boxShadow: "10px 12px 10px 0px rgba(0,0,0,0.25)",
+          <Card
+            elevation="0"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              backgroundColor: "transparent",
+              boxShadow: "none",
             }}
-          />
+          >
+            <CardContent sx={{ color: "white", mb: 2 }}>
+              <div className="vollkorn-hello">Hello !Welcome to </div>
+              <div className="vollkorn-scrutiny">Srunity Global</div>
+            </CardContent>
 
+            <CardMedia
+              component="img"
+              image={backgroundImage}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{
+                width: "42vh",
+                height: "42vh",
+                borderRadius: "50%",
+                backgroundColor: "#0cdaff",
+                ml: 6,
+                boxShadow: "10px 12px 10px 0px rgba(0,0,0,0.25)",
+              }}
+            />
 
-          <CardContent sx={{ color: "white", mt: 2 }} >
-            <div className="vollkorn-body" >
-              A CRM for CAWI project </div>
-            <div className="vollkorn-body">
-              management
-            </div>
-
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={6} md={7}>
-        <Card
-          style={{
-            // border: "2px solid black",
-            borderTopRightRadius: 0,
-            borderBottomRightRadius: 0,
-            borderTopLeftRadius: "10%",
-            borderBottomLeftRadius: "10%",
-          }}
-          sx={{
-            px: 2,
-            height: "100%",
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={logo}
-            sx={{
-              width: "10vh",
-              position: "absolute",
-              right: "1vh",
-              top: "1vh",
-              // margin: 1,
+            <CardContent sx={{ color: "white", mt: 2 }}>
+              <div className="vollkorn-body">
+                A Panel for project management
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Card
+            style={{
+              borderTopRightRadius: 0,
+              borderBottomRightRadius: 0,
+              borderTopLeftRadius: "10%",
+              borderBottomLeftRadius: "10%",
+              height: "100%",
+              position: "relative",
             }}
-          />
+          >
+            <CardContent>
+              <Typography
+                className="vollkorn-Login"
+                sx={{ fontWeight: "700", fontSize: "4vh", mt: 1 }}
+                style={{ fontFamily: "Vollkorn ,serif" }}
+              >
+                Log In
+              </Typography>
+            </CardContent>
+            <CardMedia
+              component="img"
+              image={logo}
+              sx={{
+                width: "10vh",
+                position: "absolute",
+                right: "1vh",
+                top: "1vh",
+              }}
+            />
 
-          <CardContent sx={{ mt: 13, mx: 13, mb: 4 }}>
-            <form onSubmit={handleSubmit}>
-              <CustomInputLabel
-                htmlFor="emailOrNumber"
+            <CardContent sx={{ mx: 13, mb: 4 }}>
+              <form onSubmit={handleSubmit}>
+                <CustomTextField
+                  name="username"
+                  type="text"
+                  value={username}
+                  className="arimo-input-label"
+                  label="Email or Number"
+                  sx={{ mt: 3, fontWeight: 550, fontSize: "2.5vh" }}
+                  onChange={onChangeUsername}
+                  required
+                />
+
+                {/* <CustomInputLabel
+                  htmlFor="password"
+                  className="arimo-input-label"
+                  label="Password"
+                  sx={{ mt: 3, fontWeight: 550, fontSize: "2.5vh" }}
+                /> */}
+                <CustomTextField
+                  name="password"
+                  type="password"
+                  value={password}
+                  className="arimo-input-label"
+                  label="Password"
+                  sx={{ mt: 3, fontWeight: 550, fontSize: "2.5vh" }}
+                  onChange={onChangePassword}
+                  required
+                />
+
+                <FormHelperText
+                  sx={{ color: "#175f93", mt: 2, mb: 5, fontSize: "1.5vh" }}
+                  className="arimo-input-label"
+                >
+                  forgot password?
+                </FormHelperText>
+
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                      // Responsive width
+                      width: { xs: "80%", sm: "60%", md: "40%" },
+                      borderRadius: "10px",
+                      boxShadow: "3px 3px 6px 0px #0cdaff",
+                      "&:hover": {
+                        boxShadow: "4px 4px 9px 0px #1976d2",
+                      },
+                      border: "1px solid white",
+                      background: "rgb(83,89,231)",
+                      background:
+                        "linear-gradient(274deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
+                    }}
+                  >
+                    <Typography
+                      variant="body1"
+                      fontWeight="bold"
+                      style={{ fontSize: "18px" }}
+                    >
+                      Log In
+                    </Typography>
+                  </Button>
+                </Box>
+              </form>
+            </CardContent>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              sx={{ my: 3 }}
+            >
+              <CardActions
                 className="arimo-input-label"
-                label="Email or Number"
-                sx={{ mt: 3, fontWeight: 550, fontSize: '2.5vh' }}
-              />
-
-              <CustomTextField
-                name="username"
-                type='text'
-                placeholder="Email or Number"
-                value={username}
-                onChange={onChangeUsername}
-                required
-              />
-
-              <CustomInputLabel
-                htmlFor="password"
-                className="arimo-input-label"
-                label="Password"
-                sx={{ mt: 3, fontWeight: 550, fontSize: '2.5vh' }}
-              />
-              <CustomTextField
-                name="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={onChangePassword}
-                required
-              />
-             
-              <FormHelperText sx={{ color: "#175f93", mt: 2, mb: 5, fontSize: '1.5vh' }} className="arimo-input-label">
-
-                forgot password?
-              </FormHelperText>
-
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    width: "40%",
-                    borderRadius: "10px",
-
-                    boxShadow: "3px 3px 6px 0px #0cdaff",
-                    '&:hover': {
-                      boxShadow: "4px 4px 9px 0px #1976d2",
-
-                    },
-                    border: "1px solid white",
-                    background: "rgb(83,89,231)",
-                    background:
-                      "linear-gradient(274deg, rgba(83,89,231,1) 0%, rgba(12,218,255,1) 100%)",
+                style={{ fontSize: "2vh" }}
+              >
+                {"Are you new?"}
+                <Link
+                  to="/register"
+                  style={{
+                    textDecoration: "none",
+                    fontWeight: "bold",
+                    color: "#0cdaff",
                   }}
                 >
-                  <Typography
-                    variant="body"
-                    fontWeight="bold"
-                    style={{ fontSize: "20px" }}
-                  >
-                    Sign In
-                  </Typography>
-                </Button>
-              </Box>
-            </form>
-          </CardContent>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-            sx={{ mt: 3 }}
-          >
-
-            <CardActions className="arimo-input-label" style={{ fontSize: "2vh" }}>
-
-              {"Are you new?"}
-              <Link
-                to="/register"
-                style={{
-                  textDecoration: "none",
-                  fontWeight: "bold",
-                  color: "#0cdaff",
-                }}
-              >
-                {" "}
-                Sign up here
-              </Link>
-            </CardActions>
-          </Grid>
-        </Card>
+                  {" "}
+                  Sign up here
+                </Link>
+              </CardActions>
+            </Grid>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
--- scrutinyglobal/src/Components/Parts/CustomTextField.js
+++ scrutinyglobal/src/Components/Parts/CustomTextField.js
@@ -1,36 +1,37 @@
 import { TextField, InputLabel } from "@mui/material";
-import React from 'react'
-import "../Login.css"
+import React from "react";
+import "../Login.css";
 
 // Reusable TextField Component
-export const CustomTextField = ({ name, type, placeholder, value, onChange, required }) => {
-    return (
-        <TextField
-            name={name}
-            type={type}
-            placeholder={placeholder}
-            id="standard-basic"
-            variant="standard"
-            value={value}
-            onChange={onChange}
-            sx={{ my: 3 }}
-            size="small"
-            fullWidth
-            required={required}
-        />
-    )
-}
+export const CustomTextField = ({
+  name,
+  type,
+  value,
+  onChange,
+  required,
+  className,
+  sx,
+  label,
+}) => {
+  return (
+    <>
+      <InputLabel className={className} sx={sx}>
+        {label}
+      </InputLabel>
 
-// Reusable InputLabel Component
-export const CustomInputLabel = ({ htmlFor, className, sx,label }) => {
-    return (
-        <InputLabel
-            htmlFor={htmlFor}
-            className={className}
-            sx={sx}
-        >
-            {label}
-        </InputLabel>
-    );
+      <TextField
+        name={name}
+        type={type}
+        placeholder={label}
+        id="standard-basic"
+        variant="standard"
+        value={value}
+        onChange={onChange}
+        sx={{ my: 3 }}
+        size="small"
+        fullWidth
+        required={required}
+      />
+    </>
+  );
 };
-
